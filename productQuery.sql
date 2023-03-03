438a325ac56e613c0cdecab3583074f9a4611600
VladimirPiki <vlade.piki23@hotmail.com>
2023-03-03 21:40:57 +0100

    Done!

--- productQuery.sql
+++
@@ -1,8 +0,0 @@
-#INSERT INTO product(id,`name`,price,attributes,`value`) VALUES('vla-krs-bit','newGame2022',40.01,'MB','40');
-#CALL _insert_product ('D-ELE-KRST','for her',409.33,'HxWxL','24x30x30');
-#DELETE FROM product WHERE id='D-ELE-KRST';
-#CALL _delete_product ('D-ELE-KRST')
-#SELECT * FROM product
-#CALL _select_product
-SELECT id,`name`,price,attributes,CONCAT(attributes,concat(height,"x",width,"x",`length`),concat(kg," ",Kilogrami),mb) AS `atributes` 
-FROM product
